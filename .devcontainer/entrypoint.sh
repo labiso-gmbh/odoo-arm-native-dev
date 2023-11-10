f89f262d429f65eb9dd83f56be1f73931ec3a557
Laurence Lars Labusch <lala@labiso.de>
2023-11-10 11:44:11 +0100

    [UPD] Updated Dockerfile and docker-compose.yml
    - Changed base image to debian:bookworm-slim for a smaller footprint
    - Set up environment variables for localization
    - Installed essential packages and dependencies for Odoo and PostgreSQL
    - Added installation of rtlcss using npm for RTL language support
    - Specified Odoo version as an environment variable for better maintainability
    - Installed Odoo from the official source
    - Copied essential scripts and configuration files into the container
    - Set permissions and created directories for Odoo
    - Exposed ports for Odoo services
    
    [UPD] Updated entrypoint.sh
    
    - Modified pip3 install command to break system packages when installing requirements.txt
    
    [NEW] Added wait-for-psql.py script
    
    - Script waits for PostgreSQL database connection before starting the application

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-find /workspace -name "requirements.txt" -exec pip3 install -r {} \;
+find /workspace -name "requirements.txt" -exec pip3 install --break-system-packages -r {} \;
 
 sleep infinity
